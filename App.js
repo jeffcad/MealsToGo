7c2321612781c6310a308575155e1c05d33c376a
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-03-08 21:30:31 +0900

    Add SafeAreaView and Android status bar check

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar
+} from 'react-native'
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ backgroundColor: 'lightgreen', padding: 16 }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ backgroundColor: 'lightsteelblue', flex: 1, padding: 16 }}>
+          <Text>for</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   )
 }
 
