1e0b8b3854c643e60703767c958b2062552b7c5c
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-03-13 20:08:44 +0900

    Add Firebase and authentication service/context

--- App.js
+++ App.js
@@ -9,9 +9,24 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context'
 import { LocationContextProvider } from './src/services/location/location.context'
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context'
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context'
 import { Navigation } from './src/infrastructure/navigation/index'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAsgIAIiuHFy4vsEjIQ_mHQJ8dZgQQcLDE",
+  authDomain: "meals-to-go-jeffcad.firebaseapp.com",
+  projectId: "meals-to-go-jeffcad",
+  storageBucket: "meals-to-go-jeffcad.appspot.com",
+  messagingSenderId: "926032245016",
+  appId: "1:926032245016:web:c8345675b3b5132782ae6d"
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     Oswald_400Regular,
     Lato_400Regular,
@@ -24,13 +39,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
